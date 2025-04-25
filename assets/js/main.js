27d72f429dea3cc1104992557d125dd2fd5db876
rramirez1401 <raul.ramirez1401+1@gmail.com>
2025-04-25 04:07:47 +0000

    avance

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,10 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-$('#btn').click(function(){
-    alert("presionado")
+$('#btn1').click(function(){
+    alert("El correo fue enviado correctamente...")
+})
 
+$('#btn2').click(function(){
+    alert("Añadido a favoritos :) ")
 })
